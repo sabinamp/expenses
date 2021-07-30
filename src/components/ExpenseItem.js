c7bb02172c273a9dd79de79e1fd20902468cdf34
Sabina Maria <p_sabinamaria@yahoo.com>
2021-07-30 18:43:49 +0200

    add Card

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,18 @@
 import ExpenseDate from './ExpenseDate/ExpenseDate';
 import './ExpenseItem.css';
+import Card from './layout/Card/Card';
 
 function ExpenseItem(props) {
    
     return (
         <div>         
-            <div className='expense-item'>
+            <Card className='expense-item'>
                 <ExpenseDate date={props.date}/>
                 <div className='expense-item_descr'>
                     <h2>{props.title}</h2>
                     <div className='expense-item_price'>${props.amount}</div>
                 </div>               
-            </div>
+            </Card>
         </div>
     );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,14 @@
 import ExpenseItem from "../ExpenseItem";
 import './Expenses.css';
-
+import Card from "../layout/Card/Card";
 const Expenses=(props)=>{
     const {expenses} = props;
-    return ( <div>
+    return ( 
+        <Card>
          <ExpenseItem title= {expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
          <ExpenseItem title= {expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
          <ExpenseItem title= {expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
-        </div>)
+        </Card>)
 }
 
 export default Expenses;
