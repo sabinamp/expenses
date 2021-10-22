acb7546879c747c42c1b339119d3ff86f731e6d9
Sabina Maria <p_sabinamaria@yahoo.com>
2021-10-22 20:02:40 +0200

    add select dropdown for filtering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,15 +1,25 @@
 import ExpenseItem from "../ExpenseItem/ExpenseItem";
 import "./Expenses.css";
 import Card from "../layout/Card/Card";
+import { useState } from "react";
+import ExpenseFilter from "./ExpenseFilter";
+
 const Expenses = (props) => {
 	const { expenseItems } = props;
-    
+    const [filter, setFilter] = useState('2021');
+    const onYearSelectedHandler = (year)=>{
+        setFilter(year);
+        //console.log("filter: "+filter);
+    }
 	return ( 
-            <Card>
+        <div>            
+            <Card className= "expenses">
+            <ExpenseFilter selected={filter} onYearSelectedHandler={onYearSelectedHandler}/>
             { expenseItems.map( (item)=>         
                 (<ExpenseItem key={item.id} title={item.title} amount={item.amount} date={item.date}/>)
             )}
             </Card>
+        </div>
 		
 	);
 };
