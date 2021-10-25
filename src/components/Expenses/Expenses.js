ab0770d4f0127c43d1a95b93d953de152fee2c00
Sabina Maria <p_sabinamaria@yahoo.com>
2021-10-25 10:37:53 +0200

    filtering items based on filter year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,15 +7,26 @@ import ExpenseFilter from "./ExpenseFilter";
 const Expenses = (props) => {
 	const { expenseItems } = props;
     const [filter, setFilter] = useState('2021');
+    
+    
+    const filteredItems = expenseItems.filter( item=>(
+            //console.log("filter: "+filter );
+            //console.log('Filtering...'+expenseItems.map(item=> { item.title, item.date })
+            item.date.getFullYear().toString() === filter
+        ));
+        
+    
+
     const onYearSelectedHandler = (year)=>{
-        setFilter(year);
-        //console.log("filter: "+filter);
+        setFilter(year);          
     }
+
+
 	return ( 
         <div>            
             <Card className= "expenses">
             <ExpenseFilter selected={filter} onYearSelectedHandler={onYearSelectedHandler}/>
-            { expenseItems.map( (item)=>         
+            { filteredItems.map( (item)=>         
                 (<ExpenseItem key={item.id} title={item.title} amount={item.amount} date={item.date}/>)
             )}
             </Card>
